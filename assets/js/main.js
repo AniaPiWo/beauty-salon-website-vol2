9ca074e0d65d8f6a835638dd2ac91cbf1d1f3273
AniaPiWo <snowflake@wp.pl>
2024-04-09 18:19:51 +0200

    hero, footer, gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,50 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+const iframeBtn = document.getElementById("iframeBtn");
+const iframe = document.getElementById("iframe");
+
+iframeBtn.addEventListener("click", () => {
+  iframe.classList.toggle("hidden");
+});
+
 let swiperImages = new Swiper(".home__swiper", {
   loop: true,
   spaceBetween: 64,
@@ -32,11 +76,11 @@ let swiperImages = new Swiper(".home__swiper", {
     el: ".swiper-pagination",
     type: "fraction",
   },
-  autoplay: {
+  /*   autoplay: {
     delay: 3000,
     disableOnInteraction: false,
     reverseDirection: true,
-  },
+  }, */
 
   navigation: {
     nextEl: ".swiper-button-next",
@@ -54,6 +98,25 @@ let swiperTitles = new Swiper(".home__titles", {
 swiperImages.controller.control = swiperTitles;
 swiperTitles.controller.control = swiperImages;
 
+const swiper = new Swiper("#gallery-1", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: false,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
+
 const bgHeader = () => {
   const header = document.getElementById("header");
   this.scrollY >= 50
@@ -61,3 +124,6 @@ const bgHeader = () => {
     : header.classList.remove("bg-header");
 };
 window.addEventListener("scroll", bgHeader);
+
+const year = document.getElementById("year");
+year.innerHTML = new Date().getFullYear();
